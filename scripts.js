6f67856a844dcfef2e31e56e9ff18844f2fd7650
twihell <sevlost@gmail.com>
2019-04-24 21:19:17 +0300

    do sprint2

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ function removeError(element) {
 
 
 const submit = document.getElementById('submit');
-
+const tbody = document.getElementById('tbody');
 const tasks = [];
 
 
@@ -24,7 +24,7 @@ submit.addEventListener('click', function () {
             description: description.value,
             date: `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
         }
-            
+
         tasks.push(task);
         title.value = '';
         description.value = '';
@@ -37,17 +37,32 @@ submit.addEventListener('click', function () {
             }
         }
     }
-    // setData(tasks);
+    setData(tasks);
 
     console.log('click', tasks);
-}); 
+});
 
 function setData(array) {
     if (array.length > 0) {
         let tmp = '';
-            for (let i = 0; i < array.length; i++) {
-                tmp += renderItem(array[i]);
-            }
-
+        for (let i = 0; i < array.length; i++) {
+            tmp += renderItem(i, array[i]);
+        }
+        tbody.innerHTML = tmp;
     }
+}
+
+function renderItem(i, item) {
+    return `
+    <tr>
+    <th scope="row">${i + 1}</th>
+    <td>${item.title}</td>
+    <td>${item.description}</td>
+    <td>${item.date}</td>
+    <td>
+        <i data-id="${i}" data-type="edit" class="fas fa-edit"></i>
+        <i data-id="${i}" data-type="remove" class="far fa-trash-alt"></i>
+        </td>
+    </tr>
+    `;
 }
